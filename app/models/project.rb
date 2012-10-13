a865a50b6f0823512f7548f8624906038c25a594
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-13 00:52:24 -0300

    created the #total_messages_sent in project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
   attr_accessible :description, :goal, :image_url, :name
   has_many :messages
   validates :name, :description, presence: true
+
+  def total_messages_sent
+    messages.where("volunteer_id IS NOT ?", nil).size
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,4 +9,19 @@ describe Project do
   describe "associations" do
     it { should have_many :messages }
   end
+
+  describe "#total_messages_sent" do
+    before do
+      @project = Project.make!
+      user = User.make!
+      3.times { Message.make! project: @project, author: user, volunteer: User.make! }
+      2.times { Message.make! project: @project, author: user }
+    end
+
+    subject { @project }
+
+    it "should return the total messtes sent" do
+      subject.total_messages_sent.should == 3
+    end
+  end
 end
