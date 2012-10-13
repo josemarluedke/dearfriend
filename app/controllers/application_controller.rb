e073fed3a329b8c1150aebae41dabb95b9f592d6
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-10-13 17:54:51 -0300

    Method test to redirect Rumble testers to production url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :keep_user_at_production_domain
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
   end
@@ -8,6 +10,12 @@ class ApplicationController < ActionController::Base
   protected
   def render_404
     raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def keep_user_at_production_domain
+    if /dear\-friend\.r12\.railsrumble\.com/.match(request.host)
+      redirect_to "http://dearfriend.cc/"
+    end
   end
 end
 
