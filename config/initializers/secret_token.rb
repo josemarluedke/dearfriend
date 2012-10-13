81c176577a325200aa75313e46cf070b88e09a81
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-12 22:01:06 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DearFriend::Application.config.secret_token = 'e1b70a2ce378354ec9c47cfb702e1bf10c7747220ac643c357e2bcd50f0c6abfbffbdf4efd94cbe0470303560ae96e4b32ce3f46e213d20b1de3c5ba33c4547a'
