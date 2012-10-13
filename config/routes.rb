93dab2b14a8f308b3586aa5cb6aadff83674c85d
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-12 22:02:51 -0300

    Create Users::OmniauthCallbacks and Authorizations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 DearFriend::Application.routes.draw do
+  resources :authorizations, only: [:destroy]
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
