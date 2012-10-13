9072463e322bfa2f0023acf1e937d414f2a79b84
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-13 00:14:10 -0300

    creating specs for project model

--- db/migrate/20121013015024_create_projects.rb
+++ db/migrate/20121013015024_create_projects.rb
@@ -1,9 +1,9 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-      t.string :name
-      t.text :description
-      t.decimal :goal
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :goal
       t.string :image_url
 
       t.timestamps
