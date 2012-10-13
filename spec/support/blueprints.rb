b9f1369299e4f6499bf4b776659257f707d7b7b5
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-12 22:02:51 -0300

    Create User and Authorization models.

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,9 +1,16 @@
 require 'machinist/active_record'
 
 # Add your blueprints here.
-#
-# e.g.
-#   Post.blueprint do
-#     title { "Post #{sn}" }
-#     body  { "Lorem ipsum..." }
-#   end
+
+User.blueprint do
+  name {'Josemar Davi Luedke'}
+  email {'josemarluedke@gmail.com'}
+  password {'josemar'}
+  password_confirmation {'josemar'}
+end
+
+Authorization.blueprint do
+  user {User.make!}
+  provider {"facebook"}
+  uid {10000}
+end
