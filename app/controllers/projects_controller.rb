f4b32a747d93b483000d8fc4ab07e459bee77861
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-10-13 17:08:06 -0300

    Generate and send to volunteer project's messages as a text file

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,12 +6,12 @@ class ProjectsController < ApplicationController
   # POST /projects/1/download_messages
   def download_messages
     @project = Project.find(params[:id])
-    @project.give_messages_to_volunteer(current_user,
-                                        params[:messages_quantity])
-    flash[:notice] = "Download started!"
+    messages_file = @project.give_messages_to_volunteer(current_user,
+                                                        params[:messages_quantity])
   rescue InsufficientMessagesToBeSent => e
     flash[:alert] = "There is just #{@project.total_messages_to_be_downloaded} messages to be downloaded."
-  ensure
     render :show
+  else
+    send_data messages_file, filename: "messages_#{DateTime.current}.txt"
   end
 end
