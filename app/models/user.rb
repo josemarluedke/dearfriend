c4c19af9303e21640aa757e95ebe191e29cae5e6
Rafael Barboza <barboza.cardoso@gmail.com>
2012-10-14 02:15:06 -0300

    added email confirmation when a volunteer is aprooved

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,8 +30,12 @@ class User < ActiveRecord::Base
     volunteer && verified_volunteer
   end
 
-  before_save do
-    UserMailer.volunteer_request_email(self).deliver if volunteer && !verified_volunteer rescue nil
+  before_validation do
+    saved_volunteer = id.nil? ? false : User.find(id).volunteer
+    saved_verified_volunteer = id.nil? ? false : User.find(id).verified_volunteer
+    
+    UserMailer.volunteer_request_email(self).deliver if volunteer && !saved_volunteer && !verified_volunteer rescue nil
+    UserMailer.volunteer_confirmation_email(self).deliver if verified_volunteer && !saved_verified_volunteer rescue nil
   end
 end
 
