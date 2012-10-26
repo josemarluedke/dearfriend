914b31a011f02e426d208cbbf4d4864746f2e14d
Rafael Barboza <barboza.cardoso@gmail.com>
2012-10-26 15:06:20 -0200

    commented configs for migrate

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,11 +1,11 @@
 begin
   ActionMailer::Base.smtp_settings = {
-  :address        => Setting['mail.smtp'],
-  :port           => Setting['mail.port'],
-  :authentication => :plain,
-  :user_name      => Setting['mail.username'],
-  :password       => Setting['mail.password'],
-  :domain         => Setting['mail.domain']
+  # :address        => Setting['mail.smtp'],
+  # :port           => Setting['mail.port'],
+  # :authentication => :plain,
+  # :user_name      => Setting['mail.username'],
+  # :password       => Setting['mail.password'],
+  # :domain         => Setting['mail.domain']
   }
 rescue
   nil
