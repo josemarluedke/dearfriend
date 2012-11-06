85ca476c5adc646312d9ae152dd62196ebdeb009
Rafael Barboza <barboza.cardoso@gmail.com>
2012-11-06 12:18:46 -0200

    added Setting for AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAIUTPUOVUPSDVIFVQ',       # required
-    :aws_secret_access_key  => '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',       # required
+    :aws_access_key_id      => Setting['aws.key'],       # required
+    :aws_secret_access_key  => Setting['aws.secret'],       # required
     :region                 => 'sa-east-1'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'dearfriend'
