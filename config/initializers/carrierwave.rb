abc7b9e9d3c75b86b7b95704aaa207e5001982fb
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-11-16 19:15:51 -0200

    handle error loading settings when table does not yet exist

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,13 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => Setting['aws.key'],       # required
-    :aws_secret_access_key  => Setting['aws.secret'],       # required
-    :region                 => 'sa-east-1'  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'dearfriend'
+begin
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',       # required
+      :aws_access_key_id      => Setting['aws.key'],       # required
+      :aws_secret_access_key  => Setting['aws.secret'],       # required
+      :region                 => 'sa-east-1'  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = 'dearfriend'
+  end
+rescue Exception => e
+  Rails.logger.warn "Error loading settings: #{e}"
 end
