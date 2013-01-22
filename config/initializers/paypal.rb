5313715755438af434b1e24176b9eb17061c742d
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-01-22 19:31:17 -0200

    Sandbox false

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -5,7 +5,7 @@ PAYPAL_CONFIG = if ENV['paypal_username'] # for heroku
     username:  ENV['paypal_username'],
     password:  ENV['paypal_password'],
     signature: ENV['paypal_signature'],
-    sandbox:   ENV['paypal_sandbox']
+    sandbox:  false
   }
 else
   YAML.load_file("#{Rails.root}/config/paypal.yml")[Rails.env].symbolize_keys
