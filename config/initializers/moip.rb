ba9b2e1ba9d80f74a715640d1aa71144f665a3ad
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2013-02-15 22:28:16 -0200

    Store Moip token/key using rails-settings-cached

--- config/initializers/moip.rb
+++ config/initializers/moip.rb
@@ -1,5 +1,4 @@
 Moiper.configure do |config|
-  config.sandbox = true
-  config.token   = 'EZMXMQNOSQANDBAYCQA7LUH8NMBEUKBJ'
-  config.key     = 'EF9T3EMICLBRUYGKDDUHFWAYPLIVHFGEY9XR14HV'
+  config.token = Setting['moip.token']
+  config.key   = Setting['moip.key']
 end
